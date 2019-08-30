451f285e759f3846de1d66be3d7d64b51db92f47
davcamalv <dcamalv@gmail.com>
2019-08-30 12:19:25 +0200

    Control Check | 1.0.0

---
+++ Acme-Conference/src/main/java/domain/Quolet.java
@@ -0,0 +1,99 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Quolet extends DomainEntity {
+
+	//Atributes
+	private String		title;
+	private Date		publicationMoment;
+	private String		body;
+	private String		atributoDos;
+	private boolean		finalMode;
+
+	//Relationship
+	private Ticker		ticker;
+	private Conference	conference;
+
+
+	@NotBlank
+	@SafeHtml
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+	public Date getPublicationMoment() {
+		return this.publicationMoment;
+	}
+
+	public void setPublicationMoment(final Date publicationMoment) {
+		this.publicationMoment = publicationMoment;
+	}
+
+	public String getBody() {
+		return this.body;
+	}
+
+	public void setBody(final String body) {
+		this.body = body;
+	}
+
+	public String getAtributoDos() {
+		return this.atributoDos;
+	}
+
+	public void setAtributoDos(final String atributoDos) {
+		this.atributoDos = atributoDos;
+	}
+
+	public boolean isFinalMode() {
+		return this.finalMode;
+	}
+
+	public void setFinalMode(final boolean finalMode) {
+		this.finalMode = finalMode;
+	}
+
+	@Valid
+	@OneToOne(optional = false)
+	public Ticker getTicker() {
+		return this.ticker;
+	}
+
+	public void setTicker(final Ticker ticker) {
+		this.ticker = ticker;
+	}
+
+	@Valid
+	@ManyToOne(optional = true)
+	public Conference getConference() {
+		return this.conference;
+	}
+
+	public void setConference(final Conference conference) {
+		this.conference = conference;
+	}
+
+}
