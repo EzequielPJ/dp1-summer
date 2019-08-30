64698755397edd6f3ba383b02c2a4f4a30044999
manuelc.p.g@gmail.com <manuelc.p.g@gmail.com>
2019-08-30 05:28:51 +0100

    Tests | 1.0.0

---
+++ Acme-Conference/src/test/java/services/AdministratorServiceTest.java
@@ -0,0 +1,104 @@
+
+package services;
+
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import security.Authority;
+import security.LoginService;
+import security.UserAccount;
+import utilities.AbstractTest;
+import domain.Administrator;
+
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class AdministratorServiceTest extends AbstractTest {
+
+	@Autowired
+	private AdministratorService	administratorService;
+
+
+	@Test
+	public void AdminRegisterDriver() {
+		final Object testingData[][] = {
+			{
+				null, true, IllegalArgumentException.class
+			}, {
+				"admin", true, null
+			}, {
+				"author0", true, IllegalArgumentException.class
+			}, {
+				"reviewer0", true, IllegalArgumentException.class
+			}, {
+				"admin", false, ConstraintViolationException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.AdminRegisterTemplate((String) testingData[i][0], (boolean) testingData[i][1], (Class<?>) testingData[i][2]);
+	}
+
+	// Ancillary methods ------------------------------------------------------
+
+	protected void AdminRegisterTemplate(final String username, final boolean validData, final Class<?> expected) {
+		Class<?> caught;
+
+		caught = null;
+		Administrator admin = this.administratorService.create();
+		try {
+			this.authenticate(username);
+
+			if (!validData) {
+				admin.setEmail("");
+				admin.setPhotoURL("thisisanonvalidurl");
+			} else {
+				String uniqueId = "";
+				if (username == null)
+					uniqueId = "NonAuth";
+				else
+					uniqueId = "" + (LoginService.getPrincipal().hashCode() * 31);
+				admin.setEmail("<admin" + uniqueId + "@>");
+				admin.setPhotoURL("https://tiny.url/dummyPhoto");
+			}
+			admin = AdministratorServiceTest.fillData(admin);
+			this.administratorService.save(admin);
+			this.administratorService.flush();
+			this.unauthenticate();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.checkExceptions(expected, caught);
+	}
+	private static Administrator fillData(final Administrator admin) {
+		final Administrator res = admin;
+
+		res.setAddress("Direcci�n de prueba");
+
+		res.setName("DummyBoss");
+		res.setPhoneNumber("+34 555-2342");
+
+		final String dummySurname = "Dummy Wane Dan";
+		res.setSurname(dummySurname);
+
+		final UserAccount a = new UserAccount();
+		final Authority auth = new Authority();
+		auth.setAuthority(Authority.ADMINISTRATOR);
+		a.addAuthority(auth);
+		a.setUsername("DummyTest" + res.getEmail());
+		a.setPassword("DummyPass");
+
+		res.setUserAccount(a);
+
+		return res;
+	}
+}
---
+++ Acme-Conference/src/test/java/services/ReportServiceTest.java
@@ -0,0 +1,83 @@
+
+package services;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import security.LoginService;
+import utilities.AbstractTest;
+import domain.Report;
+import domain.Submission;
+
+public class ReportServiceTest extends AbstractTest {
+
+	@Autowired
+	private ReportService		reportService;
+
+	@Autowired
+	private ReviewerService		reviewerService;
+
+	@Autowired
+	private SubmissionService	submissionService;
+
+
+	@Test
+	public void CreateReportDriver() {
+
+		final Object testingData[][] = {
+			{
+
+				"reviewer0", true, null
+			}, {
+
+				"reviewer0", false, null
+			}, {
+
+				null, false, IllegalArgumentException.class
+			}, {
+
+				"admin", true, IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.createReportTemplate((String) testingData[i][0], (boolean) testingData[i][1], (Class<?>) testingData[i][2]);
+	}
+	protected void createReportTemplate(final String beanName, final boolean validData, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+
+		try {
+			super.authenticate(beanName);
+			final Collection<Submission> submissionsAssigned = this.submissionService.getSubmissionsOfReviewer(this.reviewerService.findByPrincipal(LoginService.getPrincipal()).getId());
+
+			for (final Submission s : submissionsAssigned) {
+				this.submissionService.assignReviewer(s.getId(), this.reviewerService.findByPrincipal(LoginService.getPrincipal()).getId());
+				this.submissionService.flush();
+				final Report r = this.reportService.create(s);
+				if (validData) {
+					r.setOriginalityScore(5);
+					r.setQualityScore(5);
+					r.setReadibilityScore(5);
+					r.setDecision("ACCEPT");
+				} else {
+					r.setOriginalityScore(5);
+					r.setQualityScore(5);
+					r.setReadibilityScore(5);
+					r.setDecision("NONE");
+				}
+				this.reportService.save(r);
+				this.reportService.flush();
+				break;
+			}
+
+			super.unauthenticate();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.checkExceptions(expected, caught);
+	}
+}
